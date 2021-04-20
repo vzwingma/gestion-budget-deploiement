a6999b0d0899b7883fdea7a7ef194984ba2bc1e5
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2021-04-20 12:24:27 +0200

    extract de communs

---
+++ src/docker-compose/QUA/save_mongo.bat
@@ -0,0 +1,1 @@
+docker run --rm -it -v C:/Users/vzwingma/Documents/projets/backup:/dump mongo bash -c "mongodump --out /dump/ --host clusterdev-shard-00-00.oqiqv.mongodb.net:27017,clusterdev-shard-00-01.oqiqv.mongodb.net:27017,clusterdev-shard-00-02.oqiqv.mongodb.net:27017 --ssl --username budgetdev --password 23ed8acc18fe33d5fbfa1c73bac352a8 --authenticationDatabase admin --db v12-app-dev"
