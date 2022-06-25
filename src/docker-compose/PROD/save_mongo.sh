2c15b6f4622d91508083ba21e9d4536384ff9541
Vincent ZWINGMANN <vzwingma@users.noreply.github.com>
2022-06-25 17:35:42 +0200

    Delete save_mongo.sh

--- src/docker-compose/PROD/save_mongo.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-DOCKER_PATH=/home/vzwingmann/budget-Prod
-
-docker run --rm -it -v $DOCKER_PATH/backup:/dump mongo bash -c 'mongodump --out /dump/ --uri mongodb+srv://budget:DdLGxGZftsA5AsFL@clusterbudget.xm3fg.mongodb.net/v12-app'
