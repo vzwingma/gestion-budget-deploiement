62a96a793bed57a55a46a2539fd3470ab0036074
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2022-08-29 21:43:09 +0200

    conf docker pour NAS

--- src/docker-compose/PROD/re_start-budget.sh
+++ src/docker-compose/PROD/re_start-budget.sh
@@ -1,8 +1,8 @@
 #/bin/bash
-DOCKER_PATH=/home/vzwingma/docker-compose/PROD
+DOCKER_PATH=/volume1/Data-VinZ/docker/budget/PROD
 echo ""
 echo "## Mise à jour des images docker ##"
-docker-compose -f $DOCKER_PATH/docker-compose.yml pull
+docker compose -f $DOCKER_PATH/docker-compose.yml pull
 echo ""
 echo "## (Re)création des conteneurs ## "
-docker-compose -f $DOCKER_PATH/docker-compose.yml up --force-recreate --remove-orphans -d
+docker compose -f $DOCKER_PATH/docker-compose.yml up --force-recreate --remove-orphans -d
